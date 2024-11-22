8c45b8e883972aac1c9ae94827b98a13a63b1221
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2024-11-22 14:33:11 -0300

    First commit

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-box',
+  template: `<h5>Busacar:</h5>
+  <input type="text" class="form-control" placeholder="Buscar gifs...">
+  `
+})
+export class SearchBoxComponent {
+
+}
---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    SearchBoxComponent
+  ],
+  imports: [
+    CommonModule,
+    SearchBoxComponent,
+  ],
+  exports:[
+    HomeComponent
+  ]
+})
+export class GifsModule { }
