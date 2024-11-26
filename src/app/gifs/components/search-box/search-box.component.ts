10fe7bf4405534e4b5aaf73175fe4c4b9d1d5225
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2024-11-25 22:14:39 -0300

    kk

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search-box',
-  template: `<h5>Bucar</h5>
-  <input type="text" class="form-control" placeholder="buscar gift ">
+  template: `<h5>Buscar</h5>
+  <input type="text" class="form-control" placeholder="buscar gift " (keyup.enter)="serchTag()" #txtTaginput>
 
   `
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTaginput')
+  public tagInput!:ElementRef<HTMLInputElement>;
    constructor () {}
+
+
+   serchTag() {
+    const newTag = this.tagInput.nativeElement.value
+    console.log({newTag});
+
+
+   }
 }
